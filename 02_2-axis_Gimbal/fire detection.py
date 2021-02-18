2bdea8a4792da1893290387ad683138c0e4ef1f7
shinyongjun <77784897+shinyongjun@users.noreply.github.com>
2021-02-18 17:15:10 +0900

    Update fire detection.py

--- 02_2-axis_Gimbal/fire detection.py
+++ 02_2-axis_Gimbal/fire detection.py
@@ -1,4 +1,4 @@
-https://towardsdatascience.com/early-fire-detection-system-using-deep-learning-and-opencv-6cb60260d54a
+주소 : https://towardsdatascience.com/early-fire-detection-system-using-deep-learning-and-opencv-6cb60260d54a
 
 
 import cv2
