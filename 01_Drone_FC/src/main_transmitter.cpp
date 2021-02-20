c93fd5f551d389cf25e9685857b51e41f33432ff
kimtaeyoon <dbsl0512@gmail.com>
2021-02-20 22:56:45 +0900

    gps autonomous를 위한 채널 추가
    gps autonomous 비행을 위해 채널 2개를 추가해서 gps 호버링 + gps 기반 자율비행모드 on off 로직 추가

--- 01_Drone_FC/src/main_transmitter.cpp
+++ 01_Drone_FC/src/main_transmitter.cpp
@@ -5,27 +5,27 @@
 #define pin_CE  8
 #define pin_CSN 10
 
-// Slide Potentiometer
-#define pin_slide_t A0
+// Potentiometer
+#define pin_trans_t A0  // potentiometer
 
 // Joystick
-#define pin_pr_x A3
-#define pin_pr_y A4
-#define pin_pr_sw 2
-#define pin_ty_x A1
-#define pin_ty_y A2
-#define pin_ty_sw 3
+//#define pin_pr_sw 2
+//#define pin_ty_sw 3
+//#define pin_trans_t A4  // joystick
+#define pin_trans_r A3
+#define pin_trans_e A2
+#define pin_trans_a A1
 
 // Button
-#define pin_btn_starter 5
-#define pin_btn_stopper 4
+#define pin_btn_dji 4
+#define pin_btn_diy 5
 
 // Buzzer
 #define pin_buzzer 9
 
 // LED
-#define pin_led_tx 6
-#define pin_led_rx 7
+#define pin_led_dji 7
+#define pin_led_diy 6
 
 
 
@@ -37,30 +37,43 @@
 #include <nRF24L01.h>
 #include <SPI.h>
 
-struct Data_to_be_sent {
-    byte T;
-    byte R;
-    byte E;
-    byte A;
-};
-
 
 
 // Objects & Variables
 // ========== ========== ========== ========== 
 RF24 radio(pin_CE, pin_CSN); // CE, CSN
 const byte address[6] = "01001";
-SimpleTimer bipper;
-SimpleTimer blinker;
+SimpleTimer timer;
 Data_to_be_sent sent_data;
 
-void buzzer_bbip(){
-    digitalWrite(pin_buzzer, !digitalRead(pin_buzzer));
+int diy_toggle = 0;
+int diy_last_pressed = 0;
+int dji_toggle = 0;
+int dji_last_pressed = 0;
+
+
+
+// Struct, Functions
+// ========== ========== ========== ========== 
+struct Data_to_be_sent {
+    byte T;
+    byte R;
+    byte E;
+    byte A;
+    byte M;
+    byte X;
+};
+
+void BtnChecker(){
+    if(digitalRead(pin_btn_dji) == 0){
+        digitalWrite(pin_led_dji, !digitalRead(pin_led_dji));
+    }
+    if(digitalRead(pin_btn_diy) == 0){
+        digitalWrite(pin_led_diy, !digitalRead(pin_led_diy));
+    }
 }
 
-void Led_tx_blink(){
-    digitalWrite(pin_led_tx, !digitalRead(pin_led_tx));
-}
+
 
 // Main Flow
 // ========== ========== ========== ========== 
@@ -72,31 +85,36 @@ void setup() {
     radio.openWritingPipe(address);
     radio.setPALevel(RF24_PA_MAX);
     
+    // data packet initialize
     sent_data.T = 127;
     sent_data.R = 127;
     sent_data.E = 127;
     sent_data.A = 127;
+    sent_data.M = 0;
+    sent_data.X = 0;
 
     // pin Initializing
-    pinMode(pin_pr_sw, INPUT_PULLUP);
-    pinMode(pin_ty_sw, INPUT_PULLUP);
-    pinMode(pin_btn_starter, INPUT_PULLUP);
-    pinMode(pin_btn_stopper, INPUT_PULLUP);
+    pinMode(pin_btn_diy, INPUT_PULLUP);
+    pinMode(pin_btn_dji, INPUT_PULLUP);
     pinMode(pin_buzzer, OUTPUT);
-    pinMode(pin_led_tx, OUTPUT);
-    pinMode(pin_led_rx, OUTPUT);
+    pinMode(pin_led_diy, OUTPUT);
+    pinMode(pin_led_dji, OUTPUT);
+
+    // timer
+    timer.setInterval(500, BtnChecker);
 }
 
 void loop() {
     // timer loop
-    bipper.run();
-    blinker.run();
+    timer.run();
     
     // data refresh
-    sent_data.T = map(analogRead(pin_slide_t), 0, 1023, 0, 255);
-    sent_data.R = map(analogRead(pin_ty_x),    0, 1023, 0, 255);
-    sent_data.E = map(analogRead(pin_pr_y),    0, 1023, 0, 255);
-    sent_data.A = map(analogRead(pin_pr_x),    0, 1023, 0, 255);
+    sent_data.T = map(analogRead(pin_trans_t), 0, 1023, 0, 255);
+    sent_data.R = map(analogRead(pin_trans_r), 0, 1023, 0, 255);
+    sent_data.E = map(analogRead(pin_trans_e), 0, 1023, 0, 255);
+    sent_data.A = map(analogRead(pin_trans_a), 0, 1023, 0, 255);
+    sent_data.M = digitalRead(pin_btn_dji);
+    sent_data.X = digitalRead(pin_btn_diy);
 
     // signal transmit
     radio.write(&sent_data, sizeof(sent_data));
