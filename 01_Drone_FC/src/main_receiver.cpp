fb711926063ad3ddf768d7919147a65c5ccd1ec8
kimtaeyoon <dbsl0512@gmail.com>
2021-02-21 15:15:59 +0900

    GPS hovering, GPS autonomous 모드를 위한 변경
    채널이 6개로 증가함. 하지만  pwm 신호 발생은 5개, 하나는 내부 로직에서만 처리.

--- 01_Drone_FC/src/main_receiver.cpp
+++ 01_Drone_FC/src/main_receiver.cpp
@@ -1,13 +1,16 @@
 
-// can be pwm -> 
-// using -> 8, 10, 11, 12, 13
 // PreFix
 // ========== ========== ========== ========== 
 // nRF24L01 Module
-#define pin_T 2
-#define pin_R 3
-#define pin_E 4
-#define pin_A 5
+#define pin_T 4
+#define pin_R 5
+#define pin_E 6
+#define pin_A 7
+#define pin_M ?
+#define pin_gps_rx 2
+#define pin_gps_tx A0
+#define pin_lidar_rx 3
+#define pin_lidar_tx A1
 
 #define pin_CE  8
 #define pin_CSN 10
@@ -17,18 +20,11 @@
 // Libraries
 // ========== ========== ========== ========== 
 #include <Arduino.h>
-#include <SimpleTimer.h>
 #include <RF24.h>
 #include <nRF24L01.h>
 #include <SPI.h>
 #include <Servo.h>
-
-struct Received_data {
-    byte T;
-    byte R;
-    byte E;
-    byte A;
-};
+#include <SoftwareSerial.h>
 
 
 
@@ -36,26 +32,43 @@ struct Received_data {
 // ========== ========== ========== ========== 
 RF24 radio(pin_CE, pin_CSN); // CE, CSN
 const byte address[6] = "01001";
-SimpleTimer bipper;
-SimpleTimer blinker;
 Received_data received_data;
 Servo channel_1;
 Servo channel_2;
 Servo channel_3;
 Servo channel_4;
+Servo channel_5;
+SoftwareSerial GpsSerial(2, A3);
+SoftwareSerial LidarSerial(3, A4);
 
 int ch1_value = 0;
 int ch2_value = 0;
 int ch3_value = 0;
 int ch4_value = 0;
+int ch5_value = 0;
+int ch6_value = 0;
 
 unsigned long lastRecvTime = 0;
+
+
+
+// Struct, Functions
+// ========== ========== ========== ========== 
+struct Received_data {
+    byte T;
+    byte R;
+    byte E;
+    byte A;
+    byte M;
+    byte X;
+};
 
 void reset_the_Data(){
     received_data.T = 0;
     received_data.R = 127;
     received_data.E = 127;
     received_data.A = 127;
+    //received_data.M = 0;  // atti mode
 }
 
 void receive_the_data(){
@@ -66,14 +79,23 @@ void receive_the_data(){
 }
 
 
+
 // Main Flow
 // ========== ========== ========== ========== 
 void setup() {
+    // pwm initialize
     channel_1.attach(pin_T);
     channel_2.attach(pin_R);
     channel_3.attach(pin_E);
     channel_4.attach(pin_A);
+    channel_5.attach(pin_M);
     
+    // Seiral initialize
+    Serial.begin(9600); // communicate with Rasp
+    GpsSerial.begin(9600);
+    LidarSerial.begin(115200);
+
+
     // nRF tx setting
     radio.begin();
     radio.setAutoAck(false);
@@ -81,9 +103,8 @@ void setup() {
     radio.openReadingPipe(1, address);
     radio.setPALevel(RF24_PA_MAX);
 
+    // nRF communication start
     radio.startListening();
-
-    
 }
 
 void loop() {
@@ -100,9 +121,12 @@ void loop() {
     ch2_value = map(received_data.R, 0, 255, 1000, 2000);
     ch3_value = map(received_data.E, 0, 255, 1000, 2000);
     ch4_value = map(received_data.A, 0, 255, 1000, 2000);
-    
+    ch5_value = map(received_data.M, 0,   1, 1000, 2000);
+
+    // pwm generator
     channel_1.writeMicroseconds(ch1_value);
     channel_2.writeMicroseconds(ch2_value);
     channel_3.writeMicroseconds(ch3_value);
     channel_4.writeMicroseconds(ch4_value);
+    channel_5.writeMicroseconds(ch5_value);
 }
