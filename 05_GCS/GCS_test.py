bee9ba625b1f21add2afe98399aae45b32c42f39
kimtaeyoon <dbsl0512@gmail.com>
2021-05-16 12:19:42 +0900

    Update GCS_test.py

--- 05_GCS/GCS_test.py
+++ 05_GCS/GCS_test.py
@@ -1,11 +1,14 @@
 import sys
 import urllib.request
 from xml.etree.ElementTree import tostring
+from PyQt5 import QtSerialPort
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtSerialPort import *
 from PyQt5 import uic
 
+# Global variables
 form_class = uic.loadUiType("GCS_test.ui")[0]
 lnt = 961192.0000000002
 lat = 1961815.9999999255
@@ -30,8 +33,13 @@ class WindowClass(QMainWindow, form_class):
         self.btn_left.released.connect(self.func_btn_left_)
         self.btn_right.pressed.connect(self.func_btn_right)
         self.btn_right.released.connect(self.func_btn_right_)
+        self.btn_stop.pressed.connect(self.func_btn_stop)
 
-        self.temp_btn_refresher.clicked.connect(self.LoadSoxMap)
+        self.temp_btn_refresher.pressed.connect(self.LoadSoxMap)
+        self.btn_port_refresher.pressed.connect(self.port_refresh)
+        self.table_port.cellDoubleClicked.connect(self.connect_test)
+        self.table_port.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
 
 
     def LoadSoxMap(self):
@@ -46,6 +54,24 @@ class WindowClass(QMainWindow, form_class):
         lnt+=200
         lat+=200
 
+
+
+    def port_refresh(self):
+        self.table_port.clearContents()
+        available = QtSerialPort.QSerialPortInfo().availablePorts()
+        cur_row = 0
+        for x in available:
+            self.table_port.setItem(cur_row, 0, QTableWidgetItem(str(x.portName())))
+            self.table_port.setItem(cur_row, 1, QTableWidgetItem(str(x.description())))
+            self.table_port.setItem(cur_row, 2, QTableWidgetItem(str(x.isBusy())))
+            self.table_port.setItem(cur_row, 3, QTableWidgetItem(str(x.isValid())))
+            cur_row+=1
+
+    def connect_test(self):
+        print("port connect test")
+
+
+
     def func_btn_autopilot(self):
         if(self.btn_autopilot.isChecked()):
             print("manualpilot mode")
@@ -55,14 +81,12 @@ class WindowClass(QMainWindow, form_class):
             self.btn_left.setEnabled(True)
             self.btn_right.setEnabled(True)
             return
-
         print("autopilot mode")
         self.btn_manualpilot.toggle()
         self.btn_forward.setEnabled(False)
         self.btn_backward.setEnabled(False)
         self.btn_left.setEnabled(False)
         self.btn_right.setEnabled(False)
-
 
     def func_btn_manualpilot(self):
         if(self.btn_manualpilot.isChecked()):
@@ -73,7 +97,6 @@ class WindowClass(QMainWindow, form_class):
             self.btn_left.setEnabled(False)
             self.btn_right.setEnabled(False)
             return
-
         print("manualpilot mode")
         self.btn_autopilot.toggle()
         self.btn_forward.setEnabled(True)
@@ -81,7 +104,8 @@ class WindowClass(QMainWindow, form_class):
         self.btn_left.setEnabled(True)
         self.btn_right.setEnabled(True)
 
-    
+
+
     def func_btn_forward(self):
         print("forward go")
 
@@ -105,8 +129,18 @@ class WindowClass(QMainWindow, form_class):
         
     def func_btn_right_(self):
         print("right stop")
-
     
+    def func_btn_stop(self):
+        if(self.btn_autopilot.isChecked()):
+            self.btn_autopilot.toggle()
+            self.btn_manualpilot.toggle()
+            self.btn_forward.setEnabled(True)
+            self.btn_backward.setEnabled(True)
+            self.btn_left.setEnabled(True)
+            self.btn_right.setEnabled(True)
+        #Send Emergency Stop Signal
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
